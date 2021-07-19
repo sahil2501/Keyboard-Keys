c00c42f5ebecd80fa92824902ac37924055f47a2
sahil2501 <82953793+sahil2501@users.noreply.github.com>
2021-07-19 19:09:46 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,270 @@
+window.addEventListener("keydown", my_keydown);
+function my_keydown(e) {
+    key_pressed = e.keyCode;
+    console.log(key_pressed);
+    if (key_pressed == 27) {
+        document. getElementById("danger-note").innerHTML = "Special Key is Esc.  ASCII value is 27.";
+        console.log("Special Key is Esc.  ASCII value is 27.");
+    }
+      if (key_pressed == 112) {
+        document. getElementById("danger-note").innerHTML = "Special Key is F1" + "ASCII value is 112";
+        console.log("Special Key is F1.  ASCII value is 112.");
+      }
+      
+      if (key_pressed == 113) {
+        document. getElementById("danger-note").innerHTML = "Special Key is F2" + "ASCII value is 113";
+        console.log("Special Key is F2.  ASCII value is 113.");
+      }
+      if (key_pressed == 114) {
+        document. getElementById("danger-note").innerHTML = "Special Key is F2" + "ASCII value is 114";
+        console.log("Special Key is F3.  ASCII value is 114.");
+      }
+      if (key_pressed == 115) {
+        document. getElementById("danger-note").innerHTML = "Special Key is F3" + "ASCII value is 115";
+        console.log("Special Key is F3.  ASCII value is 115.");
+      }
+      if (key_pressed == 116) {
+        document. getElementById("danger-note").innerHTML = "Special Key is F4" + "ASCII value is 116";
+        console.log("Special Key is F4.  ASCII value is 116.");
+      }
+      if (key_pressed == 117) {
+        document. getElementById("danger-note").innerHTML = "Special Key is F5" + "ASCII value is 117";
+        console.log("Special Key is F5.  ASCII value is 117.");
+      }
+      if (key_pressed == 118) {
+        document. getElementById("danger-note").innerHTML = "Special Key is F6" + "ASCII value is 118";
+        console.log("Special Key is F6.  ASCII value is 118.");
+      }
+      if (key_pressed == 119) {
+        document. getElementById("danger-note").innerHTML = "Special Key is F7" + "ASCII value is 119";
+        console.log("Special Key is F7.  ASCII value is 119.");
+      }
+      if (key_pressed == 120) {
+        document. getElementById("danger-note").innerHTML = "Special Key is F8" + "ASCII value is 120";
+        console.log("Special Key is F8.  ASCII value is 120.");
+      }
+      if (key_pressed == 121) {
+        document. getElementById("danger-note").innerHTML = "Special Key is F9" + "ASCII value is 121";
+        console.log("Special Key is F9.  ASCII value is 120.");
+      }
+      if (key_pressed == 122) {
+        document. getElementById("danger-note").innerHTML = "Special Key is F10" + "ASCII value is 122";
+        console.log("Special Key is F10.  ASCII value is 120.");
+      }
+      if (key_pressed == 123) {
+        document. getElementById("danger-note").innerHTML = "Special Key is F11" + "ASCII value is 123";
+        console.log("Special Key is F11.  ASCII value is 120.");
+      }
+      if (key_pressed == 124) {
+        document. getElementById("danger-note").innerHTML = "Special Key is F12" + "ASCII value is 124";
+        console.log("Special Key is F12.  ASCII value is 120.");
+      }
+      if (key_pressed == 65) {
+        document. getElementById("danger-note").innerHTML = "Alphabet Key is A. ASCII value is 65.";
+        console.log("Alphabet Key is A. ASCII value is 65.");
+      }
+      if (key_pressed == 66) {
+        document. getElementById("danger-note").innerHTML = "Alphabet Key is B. ASCII value is 66.";
+        console.log("Alphabet Key is B. ASCII value is 66.");
+      }
+      if (key_pressed == 67) {
+        document. getElementById("danger-note").innerHTML = "Alphabet Key is C. ASCII value is 67.";
+        console.log("Alphabet Key is C. ASCII value is 67.");
+      }
+      if (key_pressed == 68) {
+        document. getElementById("danger-note").innerHTML = "Alphabet Key is D. ASCII value is 68.";
+        console.log("Alphabet Key is D. ASCII value is 68.");
+      }
+      if (key_pressed == 69) {
+        document. getElementById("danger-note").innerHTML = "Alphabet Key is E. ASCII value is 69.";
+        console.log("Alphabet Key is E. ASCII value is 69.");
+      }
+      if (key_pressed == 70) {
+        document. getElementById("danger-note").innerHTML = "Alphabet Key is F. ASCII value is 70.";
+        console.log("Alphabet Key is F. ASCII value is 70.");
+      }
+      if (key_pressed == 71) {
+        document. getElementById("danger-note").innerHTML = "Alphabet Key is G. ASCII value is 71.";
+        console.log("Alphabet Key is G. ASCII value is 71.");
+      }
+      if (key_pressed == 72) {
+        document. getElementById("danger-note").innerHTML = "Alphabet Key is H. ASCII value is 72.";
+        console.log("Alphabet Key is H. ASCII value is 72.");
+      }
+      if (key_pressed == 73) {
+        document. getElementById("danger-note").innerHTML = "Alphabet Key is I. ASCII value is 73.";
+        console.log("Alphabet Key is I. ASCII value is 73.");
+      }
+      if (key_pressed == 74) {
+        document. getElementById("danger-note").innerHTML = "Alphabet Key is J. ASCII value is 74.";
+        console.log("Alphabet Key is J. ASCII value is 74.");
+      }
+      if (key_pressed == 75) {
+        document. getElementById("danger-note").innerHTML = "Alphabet Key is K. ASCII value is 75.";
+        console.log("Alphabet Key is K. ASCII value is 75.");
+      }
+      if (key_pressed == 76) {
+        document. getElementById("danger-note").innerHTML = "Alphabet Key is L. ASCII value is 76.";
+        console.log("Alphabet Key is L. ASCII value is 76.");
+      }
+      if (key_pressed == 77) {
+        document. getElementById("danger-note").innerHTML = "Alphabet Key is M. ASCII value is 77.";
+        console.log("Alphabet Key is M. ASCII value is 77.");
+      }
+      if (key_pressed == 78) {
+        document. getElementById("danger-note").innerHTML = "Alphabet Key is N. ASCII value is 78.";
+        console.log("Alphabet Key is N. ASCII value is 78.");
+      }
+      if (key_pressed == 79) {
+        document. getElementById("danger-note").innerHTML = "Alphabet Key is O. ASCII value is 79.";
+        console.log("Alphabet Key is O. ASCII value is 79.");
+      }
+      if (key_pressed == 80) {
+        document. getElementById("danger-note").innerHTML = "Alphabet Key is P. ASCII value is 80.";
+        console.log("Alphabet Key is P. ASCII value is 80.");
+      }
+      if (key_pressed == 81) {
+        document. getElementById("danger-note").innerHTML = "Alphabet Key is Q. ASCII value is 81.";
+        console.log("Alphabet Key is Q. ASCII value is 81.");
+      }
+      if (key_pressed == 82) {
+        document. getElementById("danger-note").innerHTML = "Alphabet Key is R. ASCII value is 82.";
+        console.log("Alphabet Key is R. ASCII value is 82.");
+      }
+      if (key_pressed == 83) {
+        document. getElementById("danger-note").innerHTML = "Alphabet Key is S. ASCII value is 83.";
+        console.log("Alphabet Key is S. ASCII value is 83.");
+      }
+      if (key_pressed == 84) {
+        document. getElementById("danger-note").innerHTML = "Alphabet Key is T. ASCII value is 84.";
+        console.log("Alphabet Key is T. ASCII value is 84.");
+      }
+      if (key_pressed == 85) {
+        document. getElementById("danger-note").innerHTML = "Alphabet Key is U. ASCII value is 85.";
+        console.log("Alphabet Key is U. ASCII value is 85.");
+      }
+      if (key_pressed == 86) {
+        document. getElementById("danger-note").innerHTML = "Alphabet Key is V. ASCII value is 86.";
+        console.log("Alphabet Key is V. ASCII value is 86.");
+      }
+      if (key_pressed == 87) {
+        document. getElementById("danger-note").innerHTML = "Alphabet Key is W. ASCII value is 87.";
+        console.log("Alphabet Key is W. ASCII value is 7.");
+      }
+      if (key_pressed == 88) {
+        document. getElementById("danger-note").innerHTML = "Alphabet Key is X. ASCII value is 88.";
+        console.log("Alphabet Key is X. ASCII value is 88.");
+      }
+      if (key_pressed == 89) {
+        document. getElementById("danger-note").innerHTML = "Alphabet Key is Y. ASCII value is 89.";
+        console.log("Alphabet Key is Y. ASCII value is 89.");
+      }
+      if (key_pressed == 90) {
+        document. getElementById("danger-note").innerHTML = "Alphabet Key is Z. ASCII value is 90.";
+        console.log("Alphabet Key is Z. ASCII value is 90.");
+      }
+      if (key_pressed == 20) {
+          document. getElementById("danger-note").innerHTML = "Special Key is Caps Lock.  ASCII value is 20.";
+          console.log("Special Key is Caps Lock.  ASCII value is 20.");
+      }
+      if (key_pressed == 9) {
+          document. getElementById("danger-note").innerHTML = "Special Key is Tab.  ASCII value is 9.";
+          console.log("Special Key is Tab.  ASCII value is 9.");
+      }
+      if (key_pressed == 17) {
+          document. getElementById("danger-note").innerHTML = "Special Key is Ctrl.  ASCII value is 17.";
+          console.log("Special Key is Ctrl.  ASCII value is 17.");
+      }
+      if (key_pressed == 16) {
+          document. getElementById("danger-note").innerHTML = "Special Key is Shift.  ASCII value is 16.";
+          console.log("Special Key is Shift.  ASCII value is 16.");
+      }
+      if (key_pressed == 18) {
+          document. getElementById("danger-note").innerHTML = "Special Key is Alt.  ASCII value is 18.";
+          console.log("Special Key is Alt.  ASCII value is 18.");
+      }
+        if (key_pressed == 48) {
+          document. getElementById("danger-note").innerHTML = "Number Key is 0. ASCII value is 48.";
+          console.log("Number Key is 1. ASCII value is 48.");
+        }
+        if (key_pressed == 49) {
+          document. getElementById("danger-note").innerHTML = "Number Key is 1. ASCII value is 49.";
+          console.log("Number Key is 1. ASCII value is 48.");
+        }
+        if (key_pressed == 50) {
+          document. getElementById("danger-note").innerHTML = "Number Key is 2. ASCII value is 50.";
+          console.log("Number Key is 2. ASCII value is 49.");
+        }
+        if (key_pressed == 51) {
+          document. getElementById("danger-note").innerHTML = "Number Key is 3. ASCII value is 51.";
+          console.log("Number Key is 3. ASCII value is 51.");
+        }
+        if (key_pressed == 52) {
+          document. getElementById("danger-note").innerHTML = "Number Key is 4. ASCII value is 52.";
+          console.log("Number Key is 4. ASCII value is 52.");
+        }
+        if (key_pressed == 53) {
+          document. getElementById("danger-note").innerHTML = "Number Key is 5. ASCII value is 53.";
+          console.log("Number Key is 5. ASCII value is 53.");
+        }
+        if (key_pressed == 54) {
+          document. getElementById("danger-note").innerHTML = "Number Key is 6. ASCII value is 54.";
+          console.log("Number Key is 6. ASCII value is 54.");
+        }
+        if (key_pressed == 55) {
+          document. getElementById("danger-note").innerHTML = "Number Key is 7. ASCII value is 55.";
+          console.log("Number Key is 7. ASCII value is 55.");
+        }
+        if (key_pressed == 56) {
+          document. getElementById("danger-note").innerHTML = "Number Key is 8. ASCII value is 56.";
+          console.log("Number Key is 8. ASCII value is 56.");
+        }
+        if (key_pressed == 57) {
+          document. getElementById("danger-note").innerHTML = "Number Key is 9. ASCII value is 57.";
+          console.log("Number Key is 9. ASCII value is 57.");
+        }
+        if (key_pressed == 96) {
+          document. getElementById("danger-note").innerHTML = "Number Key is 0. ASCII value is 96.";
+          console.log("Number Key is 0. ASCII value is 96.");
+        }
+        if (key_pressed == 97) {
+          document. getElementById("danger-note").innerHTML = "Number Key is 1. ASCII value is 97.";
+          console.log("Number Key is 2. ASCII value is 97.");
+        }
+        if (key_pressed == 98) {
+          document. getElementById("danger-note").innerHTML = "Number Key is 2. ASCII value is 98.";
+          console.log("Number Key is . ASCII value is 57.");
+        }
+        if (key_pressed == 99) {
+          document. getElementById("danger-note").innerHTML = "Number Key is 3. ASCII value is 99.";
+          console.log("Number Key is 9. ASCII value is 57.");
+        }
+        if (key_pressed == 100) {
+          document. getElementById("danger-note").innerHTML = "Number Key is 4. ASCII value is 100.";
+          console.log("Number Key is 9. ASCII value is 57.");
+        }
+        if (key_pressed == 101) {
+          document. getElementById("danger-note").innerHTML = "Number Key is 5. ASCII value is 101.";
+          console.log("Number Key is 9. ASCII value is 57.");
+        }
+        if (key_pressed == 102) {
+          document. getElementById("danger-note").innerHTML = "Number Key is 6. ASCII value is 102.";
+          console.log("Number Key is 9. ASCII value is 57.");
+        }
+        if (key_pressed == 103) {
+          document. getElementById("danger-note").innerHTML = "Number Key is 7. ASCII value is 103.";
+          console.log("Number Key is 9. ASCII value is 57.");
+        }
+        if (key_pressed == 104) {
+          document. getElementById("danger-note").innerHTML = "Number Key is 8. ASCII value is 104.";
+          console.log("Number Key is 9. ASCII value is 57.");
+        }
+        if (key_pressed == 105) {
+          document. getElementById("danger-note").innerHTML = "Number Key is 9. ASCII value is 105.";
+          console.log("Number Key is 9. ASCII value is 57.");
+        }
+        if (key_pressed == 114) {
+            document. getElementById("danger-note").innerHTML = "Special Key is Num Lock.  ASCII value is 144.";
+            console.log("Special Key is Num Lock.  ASCII value is 144.");
+        }
+}
